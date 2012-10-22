f9a560755a7c893eceaccbfc7c614dd2167c523f
Robin Paul <robinpaulalex@live.com>
2012-10-22 02:56:52 -0700

    test

--- index.php
+++ index.php
@@ -99,9 +99,7 @@ if ($user) {
 	  
       <img src="https://graph.facebook.com/<?php echo $user; ?>/picture">
     <div>
-	<pre><?php 
-	foreach ($friends as $frns)
-	{ print_r($frns['name']); }  ?></pre>
+	<pre><?php foreach ($friends as $frns)	{ echo $frns['name']; } ?></pre>
 	</div>
 	
 	<?php else: ?>
